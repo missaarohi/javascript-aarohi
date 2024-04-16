f06f9e7e5f613b74a4d79a416441989ca8f5db1f
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-16 15:39:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer versiosn
+
+//alert(3+3) we are using node js, not browser
+console.log(3
+    
+    +3) //code readibility should be high
+
+console.log("Aarohi");
+
+let name="Aarohi" //string datatype
+let age= 20 //number 2 to power 53
+let isLoggedIn= true//boolean
+
+//null= standalone value
+//undefined= value not assigned
+//symbol=unique
+
+//object
+console.log(typeof "Aarohi");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
