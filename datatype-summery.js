3a3467541e52560faea8bb5ce1424428d72787c3
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-17 01:55:22 +0000

    datatype summery

---
+++ datatype-summery.js
@@ -0,0 +1,24 @@
+//primitive
+
+//7 types: string, number,boolean,null,undefined,symbol(for making any value unique),BigInt
+//it is dynamically type programming language
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn= false
+const temp=null
+let userEmail;
+
+const id=Symbol('123')
+const Anotherid=Symbol('123')
+
+console.log(id=== Anotherid);
+
+
+const bigNumber= 23247328361231
+//if we are passing the value same then also it is unique so it will give the output false
+
+//reference type(Non Primitive)
+
+//arrays,objects,Functions
