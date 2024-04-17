dc9e8e647398e4da418ef73492c65d88ac6bf399
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-17 01:38:27 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+//same for undefined but >= value also undefined
+//in null >= value giving is true because it is treated as assignment operator and null is treated as 0 in comparison
+
+
+//strict check===
+
+console.log("2"===2);
