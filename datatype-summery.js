336ac199b83afa83cfe0d59a23df81c12b31b1b2
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-17 11:12:50 +0000

    datatype-summery

--- datatype-summery.js
+++ datatype-summery.js
@@ -6,6 +6,7 @@
 const score=100
 const scoreValue=100.3
 
+
 const isLoggedIn= false
 const temp=null
 let userEmail;
@@ -16,9 +17,22 @@ const Anotherid=Symbol('123')
 console.log(id=== Anotherid);
 
 
-const bigNumber= 23247328361231
+const bigNumber= 232473283612313433432928928n
+//if we apply n to any number then it becomes bigInt
 //if we are passing the value same then also it is unique so it will give the output false
 
 //reference type(Non Primitive)
 
 //arrays,objects,Functions
+
+const hero= ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name:"Aarohi",
+    age: 20,
+}
+
+const myFunction=function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof scoreValue);
