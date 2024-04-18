56205f0ffb59d032badc234d1375be9346167a9f
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-18 13:15:08 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,3 +1,41 @@
 const name="Aarohi"
 const repoCount="55"
-console.log(name + repoCount + "Value");
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gamename= new String("Aarohi-hc-com")
+
+
+console.log(gamename[0]);
+console.log(gamename.__proto__); //used to know the value
+
+
+console.log(gamename.length);
+
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf("i"));
+
+
+const newString= gamename.substring(0, 4)
+console.log(newString);
+
+
+const another=gamename.slice(-8, 5)
+console.log(another);
+
+
+const newstr1="    Aar   ohi       "
+
+console.log(newstr1);
+console.log(newstr1.trim());
+
+
+const url="https://aarohi.com/aarohi%20srivastava"
+
+console.log(url.replace("%20","-"));
+
+console.log(url.includes("aaro"));
+
+console.log(gamename.split('-'));
