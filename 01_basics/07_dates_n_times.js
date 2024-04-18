98cfb43aa557555384d5defb581a967fa0237d5b
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-18 13:58:56 +0000

    date and time

---
+++ 01_basics/07_dates_n_times.js
@@ -0,0 +1,7 @@
+let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
