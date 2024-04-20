b8ee791d776e96fbd0779f5c18e18d3b963bedc8
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-20 13:42:01 +0000

    date_n_times

--- 01_basics/07_dates_n_times.js
+++ 01_basics/07_dates_n_times.js
@@ -7,6 +7,33 @@ console.log(myDate.toDateString());
 console.log(typeof myDate);
 
 
-const myNewDate= new Date(2023, 0, 24)
+// const myNewDate= new Date(2023, 0, 24)
+// const myNewDate= new Date(2023, 0, 24,3,5)
+// const myNewDate= new Date("2023-01-14")
 
-console.log(myNewDate.toDateString());
+const myNewDate= new Date("01-14-2024")
+// console.log(myNewDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myNewDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+
+
+let neewDate=new Date()
+console.log(neewDate);
+console.log(neewDate.getFullYear());
+console.log(neewDate.getHours());
+console.log(neewDate.getDate());
+console.log(neewDate.getDay());
+console.log(neewDate.getUTCFullYear());
+console.log(neewDate.getMonth()+1);
+
+
+console.log(neewDate.toLocaleString('default',{
+    weekday: "long",
+    dayPeriod: "long",
+    era: "long"
+}));
