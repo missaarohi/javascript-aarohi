0a83e1fc6a17bd0a26ca2d264ef747e9b34a7f40
Aarohi Srivastava <149547688+missaarohi@users.noreply.github.com>
2024-04-21 05:40:14 +0000

    object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,6 @@
+// const tinder=new Object() it is singelton object
+const tinder={} // it is non-singelton object
+tinder.id="123abc"
+tinder.name="Sam"
+tinder.isLoggedIn= false
+console.log(tinder);
